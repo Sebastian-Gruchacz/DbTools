9c177b71bf0ffd4bd26e4d38d60bff284aecbdce
Sebastian Gruchacz <sebastian@gruchacz.com>
2017-11-10 22:59:14 +0100

    Made extracting code.

--- src/ScriptCut/Program.cs
+++ src/ScriptCut/Program.cs
@@ -1,48 +1,88 @@
-﻿using System;
-
-namespace ScriptCut
+﻿namespace ScriptCut
 {
+    using System;
     using System.IO;
     using System.Text.RegularExpressions;
 
-    class Program
+    public static class Program
     {
+        private static readonly Regex TableStartRegex;
+        private static readonly Regex TableEndRegex;
 
-        static Regex tableStartRegex = new Regex(@"SET IDENTITY_INSERT [dbo].[TypeFormOrdersLog] ON");
-        static Regex tableEndRegex = new Regex(@"SET IDENTITY_INSERT [dbo].[TypeFormOrdersLog] OFF");
+        static Program()
+        {
+            TableEndRegex = new Regex(@"SET IDENTITY_INSERT \[dbo\]\.\[([^\]]*)\] OFF", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            TableStartRegex = new Regex(@"SET IDENTITY_INSERT \[dbo\]\.\[([^\]]*)\] ON", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        }
 
         static void Main(string[] args)
         {
-            string source = @"C:\SQL Scripts\script.sql";
-            FileInfo ifo = new FileInfo(source);
-            if (!ifo.Exists)
+            string source = @"J:\SQL Scripts\script.sql"; // default
+            if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[1]))
             {
-                Console.WriteLine("Source file does not exists!");
+                source = args[0];
+            }
+
+            FileInfo ifo;
+            try
+            {
+                ifo = new FileInfo(source);
+                if (!ifo.Exists)
+                {
+                    Console.WriteLine("Source file does not exists!");
+                    return;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("'{0}' is not a valid file name.", source);
                 return;
             }
 
-            string targetsFolder = Path.Combine(ifo.DirectoryName, ifo.Name + ".parts");
+            string targetsFolder = Path.Combine(ifo.DirectoryName, Path.GetFileNameWithoutExtension(ifo.Name) + ".parts");
             if (!Directory.Exists(targetsFolder))
             {
                 Directory.CreateDirectory(targetsFolder);
             }
 
             string currentTable = null;
+            StreamWriter sw = null;
+
             using (var sourceStream = ifo.OpenText())
             {
                 string line = null;
+
                 while ((line = sourceStream.ReadLine()) != null)
                 {
-                    if (line)
+                    if (currentTable == null)
+                    {
+                        var match = TableStartRegex.Match(line);
+                        if (match.Success)
+                        {
+                            currentTable = match.Groups[1].Value;
+                            sw = new StreamWriter(Path.Combine(targetsFolder, currentTable + ".sql"));
 
+                            Console.WriteLine("Started extracting '{0}' table script into file", currentTable);
+                        }
+                    }
+                    
+                    if (currentTable != null)
+                    {
+                        sw.WriteLine(line);
 
+                        var match = TableEndRegex.Match(line);
+                        if (match.Success)
+                        {
+                            currentTable = null;
+                            sw.Flush();
+                            sw.Close();
+                            sw = null;
+                        }
+                    }
                 }
-
-
-
             }
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("DONE!");
         }
     }
 }
