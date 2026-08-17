c691ad0da936a0e620e180082114730b69d51ecf
Sebastian Gruchacz <sebastian@gruchacz.com>
2026-08-17 17:36:57 +0200

    Added missing file.

---
+++ src/Anonymyzer/Anonymyzer.PostgreSql.Tests/DatabaseEngineRegistrationTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Anonymyzer.PostgreSql.Tests;
+
+using Anonymyzer.Base;
+using Anonymyzer.Console;
+using Anonymyzer.PostgreSql;
+using Anonymyzer.SqlServer;
+using Microsoft.Extensions.DependencyInjection;
+
+public class DatabaseEngineRegistrationTests
+{
+    [Fact]
+    public void RegistersBothSupportedDatabaseEngines()
+    {
+        using ServiceProvider provider = new ServiceCollection()
+            .AddDatabaseEngines()
+            .BuildServiceProvider();
+
+        IAnonymyzerEngineBuilder[] builders = provider
+            .GetServices<IAnonymyzerEngineBuilder>()
+            .ToArray();
+
+        Assert.Contains(builders, builder => builder is SqlServerEngineBuilder);
+        Assert.Contains(builders, builder => builder is PostgreSqlEngineBuilder);
+    }
+}
